50d06155f779e713f4ebbf0cf146727b3142d97b
kimjunho <wnsghi1@naver.com>
2021-11-26 18:29:29 +0900

    - 불필요 import 제거

--- src/main/java/com/musinsa/goods/domain/Goods.java
+++ src/main/java/com/musinsa/goods/domain/Goods.java
@@ -1,33 +1,26 @@
 package com.musinsa.goods.domain;
 
-import com.musinsa.goods.common.constants.PatternConstants;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @ApiModel(value = "상품 정보")
 @Table(name = "goods")
-public class Goods implements Serializable {
+public class Goods {
     // TODO : @Size
-    // TODO : // @Pattern(regexp = PatternConstants.NUMBER_FORMAT, message = PatternConstants.NUMBER_MSG)
     @ApiModelProperty(value = "상품번호", position = 1)
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "goods_no")
     private Long goodsNo;
-
-    // TODO : 업체 상품 번호
-    /*@ApiModelProperty(value = "업체상품번호", position = 7)
-    private String comGoodsNo;*/
 
     @ApiModelProperty(value = "상품명", required = true, position = 2)
     @NotEmpty(message = "상품명")
@@ -44,13 +37,15 @@ public class Goods implements Serializable {
     @Column(name = "com_id")
     private String comId;
 
-    @ApiModelProperty(value = "상품정보 최초등록일시", position = 5)
-    @ApiParam(hidden = true)
+    @ApiModelProperty(value = "상품정보 최초등록일시", hidden = true, position = 5)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     @Column(name = "reg_dm")
     private LocalDateTime regDm;
 
-    @ApiModelProperty(value = "상품정보 수정일시", position = 6)
-    @ApiParam(hidden = true)
+    @ApiModelProperty(value = "상품정보 수정일시", hidden = true, position = 6)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     @Column(name = "upd_dm")
     private LocalDateTime updDm;
 }
