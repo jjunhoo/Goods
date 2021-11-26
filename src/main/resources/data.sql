8de391c92c4f8544e169414079e55823bb706b4a
kimjunho <wnsghi1@naver.com>
2021-11-26 18:30:27 +0900

    - Sample 데이터 입력/수정일시 포맷 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
 -- 상품 Table > Data
--- TODO : 입력일시, 수정일시 포맷
-INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (1, '테스트 상품명1', '테스트 상품설명1', 'test1', '2021-11-22 01:51:22.000000', '2021-11-22 01:51:22.000000');
-INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (2, '테스트 상품명2', '테스트 상품설명2', 'test2', '2021-11-22 01:52:22.000000', '2021-11-22 01:52:22.000000');
-INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (3, '테스트 상품명3', '테스트 상품설명3', 'test2', '2021-11-22 01:53:22.000000', '2021-11-22 01:53:22.000000');
-INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (4, '테스트 상품명4', '테스트 상품설명4', 'test2', '2021-11-22 01:54:22.000000', '2021-11-22 01:54:22.000000');
+INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (1, '테스트 상품명1', '테스트 상품설명1', 'test1', '2021-11-22 01:51:22', '2021-11-22 01:51:22');
+INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (2, '테스트 상품명2', '테스트 상품설명2', 'test2', '2021-11-22 01:52:22', '2021-11-22 01:52:22');
+INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (3, '테스트 상품명3', '테스트 상품설명3', 'test2', '2021-11-22 01:53:22', '2021-11-22 01:53:22');
+INSERT INTO	goods(goods_no,	goods_nm, goods_cont, com_id, reg_dm, upd_dm) VALUES (4, '테스트 상품명4', '테스트 상품설명4', 'test2', '2021-11-22 01:54:22', '2021-11-22 01:54:22');
