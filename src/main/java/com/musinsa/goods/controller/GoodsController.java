7164edc5dbca2cf5672a724c4af33622058b7a1b
kimjunho <wnsghi1@naver.com>
2021-11-26 18:31:38 +0900

    - Controller > 상품 등록/수정 End-Point > @RequestBody 어노테이션 추가

--- src/main/java/com/musinsa/goods/controller/GoodsController.java
+++ src/main/java/com/musinsa/goods/controller/GoodsController.java
@@ -17,23 +17,6 @@ import javax.validation.Valid;
 public class GoodsController {
     private final GoodsService goodsService;
 
-    /* TODO : 요구사항
-     * 1. (완료) 샘플 스키마를 참고하여 스키마 완성
-     *  - TODO : 기타 테이블 추가 생성 고민
-     * 2. (완료) API 버저닝 고려
-     *  - https://restfulapi.net/versioning/
-     *  - https://www.baeldung.com/rest-versioning
-     *  - 2-1. End-Point Versioning > v1 / v2
-     *  - 2-2. Http Header Versioning
-     * 3. 비정상적인 API 호출에 대해 에러 처리가 고려되어야 함 (throttling)
-     *  - https://gardeny.tistory.com/44 (호출 횟수 제한)
-     *  - https://javacan.tistory.com/entry/ratelimiter-ratelimitj-bucket4j-intro (RateLimiter, RateLimitJ, Bucket4j)
-     *  - https://happyer16.tistory.com/entry/API-rate-limiting-request-throttling-%EA%B0%9C%EB%B0%9C%ED%95%98%EA%B8%B0 (Guava > Redis - API rate-limiting)
-     *  - 3-1. CSRF (무차별 Request) > Form 데이터를 제공하는 End-Point 인 경우
-     * 4. 상품 API 가 저장되는 저장소 혹은 방식은 추후 변경될 수 있다는 가정 필요
-     * 5. 인증은 고려하지 않는다.
-     */
-
     /**
      * 상품 등록 및 수정
      *
@@ -41,7 +24,7 @@ public class GoodsController {
      */
     @ApiOperation(value = "상품 등록/수정", response = Goods.class, notes = "상품을 등록 및 수정할 수 있습니다.")
     @PostMapping(value = "/v1/goods")
-    public ResponseEntity<ResponseObject> saveGoods(final @Valid @RequestBody Goods goods) { // TODO : 추후 @RequestBody 추가
+    public ResponseEntity<ResponseObject> saveGoods(final @Valid @RequestBody Goods goods) {
         ResponseObject responseObject = new ResponseObject().of(goodsService.saveGoods(goods));
 
         return new ResponseEntity<>(responseObject, HttpStatus.OK);
